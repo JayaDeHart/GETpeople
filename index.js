a68d8256e26f30e7e762095173694826d979e8db
Jaya DeHart <dehartj97@gmail.com>
2021-09-10 15:11:53 -0700

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Testing 123 abcde'),
+    body: JSON.stringify('If this works Im a baller'),
   };
   return response;
 };
