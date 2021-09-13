6dce93bc97a4dd8668324f1abe6ff96e06e8559e
Jaya DeHart <dehartj97@gmail.com>
2021-09-12 17:02:26 -0700

    v1

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+const dynamoose = require('dynamoose');
+
+const personSchema = new dynamoose.Schema({
+  primarykey: String,
+  name: String,
+  age: Number,
+  nickName: String,
+});
+
+const Person = dynamoose.model('lab-18-people', personSchema);
+
 exports.handler = async (event) => {
-  console.log(event);
-  return { message: 'test message' };
+  let id = event.pathParameters.id;
+  if (!id) {
+    //return all
+    let response = Person.query().exec();
+    return {
+      message: 'found',
+      person: JSON.stringify(response),
+    };
+  } else {
+    let response = Person.query({ primarykey: id }).exec();
+    return {
+      message: 'found',
+      person: JSON.stringify(response),
+    };
+  }
 };
