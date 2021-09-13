8fe9075be70ce94ad2fa202b556498f267886ec9
Jaya DeHart <dehartj97@gmail.com>
2021-09-13 10:56:13 -0700

    update query

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ const personSchema = new dynamoose.Schema({
 const Person = dynamoose.model('lab-18-people', personSchema);
 
 exports.handler = async (event) => {
-  let { id } = event.pathParameters;
-  const response = await Person.get(id);
-  return {
-    message: 'foundUser',
-    user: response,
-  };
+  if (event.pathParameters) {
+    let { id } = event.pathParameters;
+    const response = await Person.get(id);
+    return {
+      message: 'foundUser',
+      user: response,
+    };
+  } else {
+    const response = await Person.scan().exec();
+    return {
+      message: 'users',
+      user: response,
+    };
+  }
 };
